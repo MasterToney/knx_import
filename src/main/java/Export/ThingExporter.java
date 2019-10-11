6ce4e655a6c26edcbb9049d7d5e2398747824d47
Anton Hößl <e1427045@student.tuwien.ac.at>
2019-10-11 19:12:49 +0200

    add thing exporter

---
+++ src/main/java/Export/ThingExporter.java
@@ -0,0 +1,50 @@
+package Export;
+
+import Models.OpenHAB.KnxControl;
+import Util.AlphanumComparator;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public abstract class ThingExporter {
+
+    public static String ExportThings(List<KnxControl> controls) {
+        var result = new StringBuilder();
+
+        result.append(
+                "Bridge knx:ip:bridge [\n" +
+                "    type=\"TUNNEL\", \n" +
+                "    ipAddress=\"192.168.0.xxx\", \n" +
+                "    portNumber=3671, \n" +
+                "    localIp=\"\",\n" +
+                "    readingPause=50, \n" +
+                "    responseTimeout=10, \n" +
+                "    readRetriesLimit=3, \n" +
+                "    autoReconnectPeriod=60,\n" +
+                "    localSourceAddr=\"0.0.0\"\n" +
+                "] {\n" +
+                "    Thing device ets [\n" +
+                "        address=\"0.0.0\",\n" +
+                "        fetch=true,\n" +
+                "        pingInterval=300,\n" +
+                "        readInterval=3600\n" +
+                "    ] {\n");
+
+        var sortedControls = controls.stream()
+                .sorted(Comparator.comparing(KnxControl::getName, new AlphanumComparator()))
+                .collect(Collectors.toList());
+
+        for (var control: sortedControls) {
+
+            result.append("        ");
+            result.append(control.toThingFormat());
+            result.append("\n");
+        }
+
+        result.append("    }\n}\n");
+
+        return result.toString();
+    }
+
+}
