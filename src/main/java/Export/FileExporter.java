87bab5e5396d4169bb72f0e7117c826812e99825
Anton Hößl <e1427045@student.tuwien.ac.at>
2019-10-11 19:02:46 +0200

    Add file exporter which writes content to a file

---
+++ src/main/java/Export/FileExporter.java
@@ -0,0 +1,23 @@
+package Export;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+
+public abstract class FileExporter {
+
+    public static void WriteToFile(Path file, String content) {
+
+
+        try {
+            Files.write(file, content.getBytes(),
+                    StandardOpenOption.CREATE,
+                    StandardOpenOption.TRUNCATE_EXISTING );
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
