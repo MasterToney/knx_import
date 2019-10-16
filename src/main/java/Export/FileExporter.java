e25d5e1e44f70e276572bb76573845bc4af8cac2
Anton Hößl <e1427045@student.tuwien.ac.at>
2019-10-16 20:30:51 +0200

    implement procedure to generate all files needed for an complete import

--- src/main/java/Export/FileExporter.java
+++ src/main/java/Export/FileExporter.java
@@ -1,9 +1,13 @@
 package Export;
 
+import Models.OpenHAB.KnxControl;
+
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
+import java.util.List;
 
 public final class FileExporter {
 
@@ -20,6 +24,36 @@ public final class FileExporter {
             e.printStackTrace();
         }
 
+    }
+
+    public static void WriteImport(Path confDirectoryPath, List<KnxControl> knxControls) {
+
+        WriteItemsFile(confDirectoryPath, knxControls);
+        WriteThingsFile(confDirectoryPath, knxControls);
+        WriteSitemapFile(confDirectoryPath, knxControls);
+
+    }
+
+    private static void WriteSitemapFile(Path confDirectoryPath, List<KnxControl> knxControls) {
+
+        var itemsFilePath = Paths.get(confDirectoryPath.toString(), "sitemaps", "knx-import.sitemap");
+
+        WriteToFile(itemsFilePath, SitemapExporter.ExportSitemap(knxControls));
+    }
+
+    private static void WriteItemsFile(Path confDirectoryPath, List<KnxControl> knxControls) {
+
+        var itemsFilePath = Paths.get(confDirectoryPath.toString(), "items", "knx-import.items");
+
+        WriteToFile(itemsFilePath, ItemExporter.ExportItems(knxControls));
+    }
+
+    private static void WriteThingsFile(Path confDirectoryPath, List<KnxControl> knxControls) {
+
+        var itemsFilePath = Paths.get(confDirectoryPath.toString(), "things", "knx-import.things");
+
+
+        WriteToFile(itemsFilePath, ThingExporter.ExportThings(knxControls));
     }
 
 }
